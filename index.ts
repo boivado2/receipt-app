2bbc049e17c4e159b6c7df051090d25587997ac7
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2023-02-28 00:06:39 +0100

    enable loging globally.

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import express from 'express'
 import vendors from './routes/vendors'
 import products from './routes/products'
+import auth from './routes/auth'
 
+import dotenv from 'dotenv'
 import db from './starter/db'
+dotenv.config()
 
 
 const app = express()
 db('mongodb://localhost/invoice')
 
 app.use(express.json());
+app.use('/api/login',auth )
 app.use('/api/vendors', vendors)
 app.use('/api/products', products)
 
---
+++ routes/auth.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from 'express';
+import  Joi from 'joi';
+import { Vendor } from '../model/vendor';
+import bcrypt from 'bcrypt'
+import jsonwebtoken  from 'jsonwebtoken'
+const router = express.Router()
+
+interface ILogin {
+  email: string
+  password: string
+}
+
+router.post('/', async(req: Request, res: Response) => {
+
+  const body = req.body as ILogin
+  
+  const { error } = validate(body)
+  if (error) return res.status(400).json({ error: error.message })
+  
+  let vendor = await Vendor.findOne({ email: body.email })
+  if (!vendor) return res.status(400).json({ error: "Invalid email or password" })
+  
+  const validPassword = await bcrypt.compare(body.password, vendor.password)
+  if (!validPassword) return res.status(400).json({ error: "Invalid email or password" })
+  
+  
+  const token = jsonwebtoken.sign({ email: vendor.email, _id: vendor._id }, process.env.jsonWebToken!)
+
+  res.json({token})
+ 
+})
+
+
+const validate = (data: ILogin) => {
+  const schema = Joi.object<ILogin>({ email: Joi.string().email().required(), password: Joi.string().required() })
+
+  return schema.validate(data, { abortEarly: true })
+  
+  
+}
+
+
+export default router
